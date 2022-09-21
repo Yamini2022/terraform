475ed6357cf56804747f1b75c95459863e99a735
Yamini2022 <105438142+Yamini2022@users.noreply.github.com>
2022-09-21 21:46:54 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAWJWHZHHVCARDMM5T"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-2"
+  }
