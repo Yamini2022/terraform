8068ab8ea3c5b1c229c5f84af8ba0ceab0aacdf5
Yamini2022 <105438142+Yamini2022@users.noreply.github.com>
2022-09-22 12:45:50 +0100

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  default = "AKIAWJWHZHHVCARDMM5T"
-  }
-variable "secret_key" {
-  default = "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"
-  }
-variable "region" {
-  default = "us-east-2"
-  }
